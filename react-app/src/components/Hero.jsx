e2c314137b2546058402b6c5612e4c5dc9c3d3c7
Eugene <87051223+Swordaac@users.noreply.github.com>
2024-07-25 14:35:58 -0400

    card--image and hero CSS

--- react-app/src/components/Hero.jsx
+++ react-app/src/components/Hero.jsx
@@ -1,13 +1,16 @@
-import React from "react"
+import React from "react";
+
 const hero = new URL(`../images/photo-grid.png`, import.meta.url).href;
 
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={hero} className="hero--photo" />
-            <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by 
-            one-of-a-kind hosts—all without leaving home.</p>
+            <img src={hero} className="hero--photo" alt="Hero" />
+            <div className="hero--content">
+                <h1 className="hero--header">Online Experiences</h1>
+                <p className="hero--text">Join unique interactive activities led by 
+                one-of-a-kind hosts—all without leaving home.</p>
+            </div>
         </section>
-    )
+    );
 }
